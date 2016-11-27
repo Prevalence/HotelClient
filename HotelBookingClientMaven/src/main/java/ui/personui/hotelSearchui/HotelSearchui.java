d2d3aa3c6bd33623c5618ffb647283c2f6713b6c
xiekailian <460715662@qq.com>
2016-11-27 18:07:04 +0800

    同步界面改变

--- HotelBookingClientMaven/src/main/java/ui/personui/hotelSearchui/HotelSearchui.java
+++ HotelBookingClientMaven/src/main/java/ui/personui/hotelSearchui/HotelSearchui.java
@@ -1,20 +1,25 @@
 package ui.personui.hotelSearchui;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class HotelSearchui extends Pane {
 	private Stage primaryStage;
 
+	private String personname;
+
 	private HotelSearchuiController hotelSearchuiController;
+
 	/**
 	 * 接受primarystage用来完成界面最小化和可移动化设置
 	 * 
 	 * @param primaryStage
 	 */
-	public HotelSearchui(Stage primaryStage,String personname) {
+	public HotelSearchui(Stage primaryStage, String personname) {
 		this.primaryStage = primaryStage;
+		this.personname = personname;
 		initHotelSearchui();
 	};
 
@@ -25,13 +30,16 @@ public class HotelSearchui extends Pane {
 		// 设置新的Pane
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HotelSearchui.fxml"));
 		loader.setRoot(this);
-		
-		hotelSearchuiController = loader.getController();
-		hotelSearchuiController.launchStage(primaryStage);
 		try {
 			loader.load();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+//		Scene scene = new Scene(this);
+//		primaryStage.setScene(scene);
+		hotelSearchuiController = loader.getController();
+
+		hotelSearchuiController.launchStage(primaryStage);
+		
 	}
 }
