8146fd548093af80223f5ccb2b058ea6febcf7d5
wuxiufeng <745040090@qq.com>
2016-12-09 20:29:36 +0800

    修改CircleWebPromotionPO与vo

--- HotelBookingClientMaven/src/main/java/po/promotionpo/webpromotionpo/CircleWebproPO.java
+++ HotelBookingClientMaven/src/main/java/po/promotionpo/webpromotionpo/CircleWebproPO.java
@@ -1,5 +1,6 @@
 package po.promotionpo.webpromotionPO;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 import po.PromotionPO;
@@ -8,13 +9,13 @@ import vo.promotionvo.webpromotionVO.CircleWebproVO;
 
 /**
  *  @author 武秀峰
- *  网站促销策略：VIP会员特定商圈专属折扣（不不同商同等级圈折扣不一样）*/
-public class CircleWebproPO extends PromotionPO {
+ *  网站促销策略：VIP会员特定商圈专属折扣（不同商圈不同等级圈折扣不一样）*/
+public class CircleWebproPO extends PromotionPO {	
 //	private String promotionType="CircleWebPromotion";
 	/*会员等级、商圈、折扣为一组，即vipLevel[i]、circle[i]、discount[i]为对应一组某等级的会员在某商圈的折扣*/
-	private ArrayList<Integer> vipLevel;
-	private ArrayList<String> circle;//商圈
-	private ArrayList<Integer> discount;//如打九五折时，discount=95
+	private int vipLevel;
+	private String circle;//商圈
+	private int discount;//如打九五折时，discount=95
 	private static final long serialVersionUID = 1L;
 	
 	public CircleWebproPO() {
@@ -22,7 +23,7 @@ public class CircleWebproPO extends PromotionPO {
 	}
 	
 	public CircleWebproPO(String promotionID, String promotionName, String promotionType,
-			String hotelnameOrWeb,ArrayList<Integer> vipLevel, ArrayList<String> circle, ArrayList<Integer> discount) {
+			String hotelnameOrWeb,int vipLevel, String circle, int discount) {
 		super.setPromotionID(promotionID);
 		super.setPromotionName(promotionName);
 		super.setPromotionType(promotionType);
@@ -31,25 +32,32 @@ public class CircleWebproPO extends PromotionPO {
 		this.circle = circle;
 		this.discount = discount;
 	}
-	public ArrayList<Integer> getVipLevel() {
+
+	
+	public int getVipLevel() {
 		return vipLevel;
 	}
-	public void setVipLevel(ArrayList<Integer> vipLevel) {
+
+	public void setVipLevel(int vipLevel) {
 		this.vipLevel = vipLevel;
 	}
-	public ArrayList<String> getCircle() {
+
+	public String getCircle() {
 		return circle;
 	}
-	public void setCircle(ArrayList<String> circle) {
+
+	public void setCircle(String circle) {
 		this.circle = circle;
 	}
-	public ArrayList<Integer> getDiscount() {
+
+	public int getDiscount() {
 		return discount;
 	}
-	public void setDiscount(ArrayList<Integer> discount) {
+
+	public void setDiscount(int discount) {
 		this.discount = discount;
 	}
-	
+
 	public PromotionVO toVO(PromotionPO po){
 		CircleWebproVO vo=new CircleWebproVO();
 		vo.setPromotionID(po.getPromotionID());
--- HotelBookingClientMaven/src/main/java/vo/promotionvo/webpromotionVO/CircleWebproVO.java
+++ HotelBookingClientMaven/src/main/java/vo/promotionvo/webpromotionVO/CircleWebproVO.java
@@ -14,16 +14,17 @@ import vo.promotionvo.hotelpromotionVO.BirthdayHotelproVO;
 public class CircleWebproVO extends PromotionVO{
 //	private String promotionType="CircleWebPromtion";
 	/*会员等级、商圈、折扣为一组，即vipLevel[i]、circle[i]、discount[i]为对应一组某等级的会员在某商圈的折扣*/
-	private ArrayList<Integer> vipLevel;
-	private ArrayList<String> circle;//商圈
-	private ArrayList<Integer> discount;//如打九五折时，discount=95
+	private int vipLevel;
+	private String circle;//商圈
+	private int discount;//如打九五折时，discount=95
+	private static final long serialVersionUID = 1L;
 	
 	public CircleWebproVO() {
 		super();
 	}
 	
 	public CircleWebproVO(String promotionID, String promotionName, String promotionType,
-			String hotelnameOrWeb,ArrayList<Integer> vipLevel, ArrayList<String> circle, ArrayList<Integer> discount) {
+			String hotelnameOrWeb,int vipLevel, String circle, int discount) {
 		super.setPromotionID(promotionID);
 		super.setPromotionName(promotionName);
 		super.setPromotionType(promotionType);
@@ -32,22 +33,29 @@ public class CircleWebproVO extends PromotionVO{
 		this.circle = circle;
 		this.discount = discount;
 	}
-	public ArrayList<Integer> getVipLevel() {
+
+	
+	public int getVipLevel() {
 		return vipLevel;
 	}
-	public void setVipLevel(ArrayList<Integer> vipLevel) {
+
+	public void setVipLevel(int vipLevel) {
 		this.vipLevel = vipLevel;
 	}
-	public ArrayList<String> getCircle() {
+
+	public String getCircle() {
 		return circle;
 	}
-	public void setCircle(ArrayList<String> circle) {
+
+	public void setCircle(String circle) {
 		this.circle = circle;
 	}
-	public ArrayList<Integer> getDiscount() {
+
+	public int getDiscount() {
 		return discount;
 	}
-	public void setDiscount(ArrayList<Integer> discount) {
+
+	public void setDiscount(int discount) {
 		this.discount = discount;
 	}
 
