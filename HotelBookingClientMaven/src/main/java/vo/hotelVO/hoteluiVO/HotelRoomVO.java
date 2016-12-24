1663e9b221820a6f682b193d6f4596510de2ab37
xiekailian <460715662@qq.com>
2016-12-24 20:40:50 +0800

    暂时同步

--- HotelBookingClientMaven/src/main/java/vo/hotelVO/hoteluiVO/HotelRoomVO.java
+++ HotelBookingClientMaven/src/main/java/vo/hotelVO/hoteluiVO/HotelRoomVO.java
@@ -4,11 +4,13 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class HotelRoomVO {
 	private final SimpleStringProperty roomtype;
+	private final SimpleStringProperty totalNumber;
 	private final SimpleStringProperty roomPrice;
 	
 	public HotelRoomVO(String roomtype, int roomPrice,int totalNumber) {
         this.roomtype = new SimpleStringProperty(roomtype);
         this.roomPrice = new SimpleStringProperty(String.valueOf(roomPrice));
+        this.totalNumber = new SimpleStringProperty(String.valueOf(totalNumber));
     }
 	
 	 public String getRoomtype() {
@@ -25,5 +27,13 @@ public class HotelRoomVO {
 
 	    public void setRoomPrice(int content) {
 	        roomPrice.set(String.valueOf(content));
+	    }
+	    
+	    public String getTotalNumber() {
+	        return totalNumber.get();
+	    }
+
+	    public void setTotalNumber(int content) {
+	    	totalNumber.set(String.valueOf(content));
 	    }
 }
